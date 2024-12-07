7094efec8b3992bd3652c18de86ff7723bfda7b0
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2024-12-07 11:56:02 +0700

    first commit

---
+++ javascript/nav-mobile.js
@@ -0,0 +1,13 @@
+document.querySelectorAll(".menu__item").forEach((item) => {
+  item.addEventListener("click", function () {
+    const checkboxId = this.dataset.checkbox;
+    const checkbox = document.getElementById(checkboxId);
+    checkbox.checked = !checkbox.checked;
+    // const url = this.dataset.url;
+    // if (checkbox) {
+    //     checkbox.checked = true;
+    //   }
+
+    //   window.location.href = url;
+  });
+});
